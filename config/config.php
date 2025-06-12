daf1431f3d3dbe23ecff8cc9b771cd647a87c77a
ledeunf <jfld791000@gmail.com>
2025-06-12 15:42:43 +0200

    update config

--- config/config.php
+++ config/config.php
@@ -34,6 +34,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+
 if ($env=="local"){
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'userirregularverbes');
